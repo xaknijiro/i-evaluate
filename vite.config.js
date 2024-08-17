23a97f674ead14ed3b583702275af94813ae7b5b
Dave Medrano <evadonardem@gmail.com>
2024-08-17 18:41:08 +0800

    Application setting updates.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        https: false,
+        host: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+        },
+    },
     plugins: [
         react(),
         laravel({
